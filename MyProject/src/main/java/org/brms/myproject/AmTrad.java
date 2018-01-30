a16395709c360c89ed2ec58b2a199e08c07cf90d
brmsAdmin <>
2018-01-30 12:48:37 +0000

    {/MyProject/src/main/java/org/brms/myproject/AmTrad.java}

--- MyProject/src/main/java/org/brms/myproject/AmTrad.java
+++ MyProject/src/main/java/org/brms/myproject/AmTrad.java
@@ -23,9 +23,41 @@ public class AmTrad implements java.io.Serializable
 
    private java.lang.String dogBreeds;
 
+   private java.lang.String plumbingType;
+
    private java.lang.Integer plumbingUpdate;
 
-   private java.lang.String plumbingType;
+   private java.lang.Integer otherStructureLimit;
+
+   private java.lang.String liabilityClaims;
+
+   private java.lang.Integer compositionShingle;
+
+   private java.lang.Integer architectualShingle;
+
+   private java.lang.Integer metal;
+
+   private java.lang.Integer tile;
+
+   private java.lang.Integer noPriorCoverage;
+
+   private Boolean farmAnimals;
+
+   private java.lang.String LLC;
+
+   private java.lang.String divingBoard;
+
+   private java.lang.String poolSlide;
+
+   private java.lang.Boolean trampoline;
+
+   private java.lang.Integer waterCoverage;
+
+   private java.lang.String insuranceScore;
+
+   private java.lang.String effectiveYearBuilt;
+
+   private java.lang.String structureType;
 
    public AmTrad()
    {
@@ -101,16 +133,6 @@ public class AmTrad implements java.io.Serializable
       this.dogBreeds = dogBreeds;
    }
 
-   public java.lang.Integer getPlumbingUpdate()
-   {
-      return this.plumbingUpdate;
-   }
-
-   public void setPlumbingUpdate(java.lang.Integer plumbingUpdate)
-   {
-      this.plumbingUpdate = plumbingUpdate;
-   }
-
    public java.lang.String getPlumbingType()
    {
       return this.plumbingType;
@@ -121,11 +143,190 @@ public class AmTrad implements java.io.Serializable
       this.plumbingType = plumbingType;
    }
 
+   public java.lang.Integer getPlumbingUpdate()
+   {
+      return this.plumbingUpdate;
+   }
+
+   public void setPlumbingUpdate(java.lang.Integer plumbingUpdate)
+   {
+      this.plumbingUpdate = plumbingUpdate;
+   }
+
+   public java.lang.Integer getOtherStructureLimit()
+   {
+      return this.otherStructureLimit;
+   }
+
+   public void setOtherStructureLimit(java.lang.Integer otherStructureLimit)
+   {
+      this.otherStructureLimit = otherStructureLimit;
+   }
+
+   public java.lang.String getLiabilityClaims()
+   {
+      return this.liabilityClaims;
+   }
+
+   public void setLiabilityClaims(java.lang.String liabilityClaims)
+   {
+      this.liabilityClaims = liabilityClaims;
+   }
+
+   public java.lang.Integer getCompositionShingle()
+   {
+      return this.compositionShingle;
+   }
+
+   public void setCompositionShingle(java.lang.Integer compositionShingle)
+   {
+      this.compositionShingle = compositionShingle;
+   }
+
+   public java.lang.Integer getArchitectualShingle()
+   {
+      return this.architectualShingle;
+   }
+
+   public void setArchitectualShingle(java.lang.Integer architectualShingle)
+   {
+      this.architectualShingle = architectualShingle;
+   }
+
+   public java.lang.Integer getMetal()
+   {
+      return this.metal;
+   }
+
+   public void setMetal(java.lang.Integer metal)
+   {
+      this.metal = metal;
+   }
+
+   public java.lang.Integer getTile()
+   {
+      return this.tile;
+   }
+
+   public void setTile(java.lang.Integer tile)
+   {
+      this.tile = tile;
+   }
+
+   public java.lang.Integer getNoPriorCoverage()
+   {
+      return this.noPriorCoverage;
+   }
+
+   public void setNoPriorCoverage(java.lang.Integer noPriorCoverage)
+   {
+      this.noPriorCoverage = noPriorCoverage;
+   }
+
+   public java.lang.String getLLC()
+   {
+      return this.LLC;
+   }
+
+   public void setLLC(java.lang.String LLC)
+   {
+      this.LLC = LLC;
+   }
+
+   public java.lang.String getDivingBoard()
+   {
+      return this.divingBoard;
+   }
+
+   public void setDivingBoard(java.lang.String divingBoard)
+   {
+      this.divingBoard = divingBoard;
+   }
+
+   public java.lang.String getPoolSlide()
+   {
+      return this.poolSlide;
+   }
+
+   public void setPoolSlide(java.lang.String poolSlide)
+   {
+      this.poolSlide = poolSlide;
+   }
+
+   public java.lang.Boolean getTrampoline()
+   {
+      return this.trampoline;
+   }
+
+   public void setTrampoline(java.lang.Boolean trampoline)
+   {
+      this.trampoline = trampoline;
+   }
+
+   public java.lang.Integer getWaterCoverage()
+   {
+      return this.waterCoverage;
+   }
+
+   public void setWaterCoverage(java.lang.Integer waterCoverage)
+   {
+      this.waterCoverage = waterCoverage;
+   }
+
+   public java.lang.String getInsuranceScore()
+   {
+      return this.insuranceScore;
+   }
+
+   public void setInsuranceScore(java.lang.String insuranceScore)
+   {
+      this.insuranceScore = insuranceScore;
+   }
+
+   public java.lang.String getEffectiveYearBuilt()
+   {
+      return this.effectiveYearBuilt;
+   }
+
+   public void setEffectiveYearBuilt(java.lang.String effectiveYearBuilt)
+   {
+      this.effectiveYearBuilt = effectiveYearBuilt;
+   }
+
+   public java.lang.Boolean getFarmAnimals()
+   {
+      return this.farmAnimals;
+   }
+
+   public void setFarmAnimals(java.lang.Boolean farmAnimals)
+   {
+      this.farmAnimals = farmAnimals;
+   }
+
+   public java.lang.String getStructureType()
+   {
+      return this.structureType;
+   }
+
+   public void setStructureType(java.lang.String structureType)
+   {
+      this.structureType = structureType;
+   }
+
    public AmTrad(java.lang.Integer yearBuilt, java.lang.String secured_community,
          java.lang.Integer prior_Claims, java.lang.String trampolinefence,
          java.lang.String flatRoofPercent, java.lang.String masonryFrame,
-         java.lang.String dogBreeds, java.lang.Integer plumbingUpdate,
-         java.lang.String plumbingType)
+         java.lang.String dogBreeds, java.lang.String plumbingType,
+         java.lang.Integer plumbingUpdate,
+         java.lang.Integer otherStructureLimit,
+         java.lang.String liabilityClaims, java.lang.Integer compositionShingle,
+         java.lang.Integer architectualShingle, java.lang.Integer metal,
+         java.lang.Integer tile, java.lang.Integer noPriorCoverage,
+         java.lang.Boolean farmAnimals, java.lang.String LLC,
+         java.lang.String divingBoard, java.lang.String poolSlide,
+         java.lang.Boolean trampoline, java.lang.Integer waterCoverage,
+         java.lang.String insuranceScore, java.lang.String effectiveYearBuilt,
+         java.lang.String structureType)
    {
       this.yearBuilt = yearBuilt;
       this.secured_community = secured_community;
@@ -134,8 +335,24 @@ public class AmTrad implements java.io.Serializable
       this.flatRoofPercent = flatRoofPercent;
       this.masonryFrame = masonryFrame;
       this.dogBreeds = dogBreeds;
-      this.plumbingUpdate = plumbingUpdate;
       this.plumbingType = plumbingType;
+      this.plumbingUpdate = plumbingUpdate;
+      this.otherStructureLimit = otherStructureLimit;
+      this.liabilityClaims = liabilityClaims;
+      this.compositionShingle = compositionShingle;
+      this.architectualShingle = architectualShingle;
+      this.metal = metal;
+      this.tile = tile;
+      this.noPriorCoverage = noPriorCoverage;
+      this.farmAnimals = farmAnimals;
+      this.LLC = LLC;
+      this.divingBoard = divingBoard;
+      this.poolSlide = poolSlide;
+      this.trampoline = trampoline;
+      this.waterCoverage = waterCoverage;
+      this.insuranceScore = insuranceScore;
+      this.effectiveYearBuilt = effectiveYearBuilt;
+      this.structureType = structureType;
    }
 
 }
